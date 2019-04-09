55be659ce0835469ab20c8d80d989801a9145f5e
boris <boris.fyot@gmail.com>
2019-04-09 09:19:38 +0200

    ajout du script ssh

---
+++ projet_perso/prometheus/ssh.sh
@@ -0,0 +1,34 @@
+﻿#!/bin/bash
+
+#######################################################################################################################
+#	V1 28/03/2019 : création du programme.                                                                        #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#######################################################################################################################
+#Debut
+#valorisation du user avec le premier argument.
+
+
+adr_ip=$1
+count=$#
+
+#Si IP vide redemander
+echo " BIENVENUE, dans l'outil de clé SSH. "
+echo "-------------------------------------"
+echo ""
+
+while [ ! "$adr_ip" ]
+do	
+	read -p "Saisir IP  a envoyer la clé ?" adr_ip
+        count=1
+done
+
+user=`users  | cut -d' ' -f 1`
+
+ssh-keygen
+
+ssh-copy-id -i ~/.ssh/id_rsa.pub "$user@$adr_ip"
