273ef093c57d6e811eddb8893fb0377565b40c26
root <root@localhost.localdomain>
2019-04-24 14:02:06 +0200

    install VB

---
+++ projet_perso/virtualbox/installVB.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#######################################################################################################################
+#	V1 28/03/2019 : création du programme.                                                                        #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#######################################################################################################################
+
+##Déclaration des variables.
+
+
+#Indicator for download source
+havetowget=0
+
+#Source
+source="https://www.virtualbox.org/download/oracle_vbox.asc"
+sourcerepo="https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo"
+adresse=`pwd`
+
+echo "(wget -q $source -O- | rpm --import -)"
+if !(wget $source)
+then 
+	echo "erreur download"
+	exit 1
+fi
+
+if !(rpm --import oracle_vbox.asc)
+then
+	echo "erreur rpm"
+	exit 3
+fi
+
+if !(ls /etc/yum.repos.d/)
+then
+	mkdir  /etc/yum.repos.d/
+fi
+
+cd  /etc/yum.repos.d/
+
+if !(wget $sourcerepo)
+then
+	echo "erreur download 2"
+	exit 2
+fi 
+
+#yum install
+yum install VirtualBox-6.0
+
+rm oracle_vbox.asc
