c7a78b8c7f9604f8364d863dcf64c8b5e68461ec
root <root@localhost.localdomain>
2019-04-25 10:46:52 +0200

    test virtualbox

--- projet_perso/vagrant/installvagrant.sh
+++ projet_perso/vagrant/installvagrant.sh
@@ -10,55 +10,7 @@
 #######################################################################################################################
 
 ##Déclaration des variables.
+user="adminl"
+source="https://releases.hashicorp.com/vagrant/1.9.6/vagrant_1.9.6_x86_64.rpm"
 
-
-#Indicator for download source
-havetowget=0
-
-#Source
-source="https://www.virtualbox.org/download/oracle_vbox.asc"
-sourcerepo="https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo"
-adresse=`pwd`
-
-echo "(wget -q $source -O- | rpm --import -)"
-if !(wget $source)
-then 
-	echo "erreur download"
-	exit 1
-fi
-
-if !(rpm --import oracle_vbox.asc)
-then
-	echo "erreur rpm"
-	exit 3
-fi
-
-if !(ls /etc/yum.repo.d/)
-then
-	mkdir  /etc/yum.repo.d/
-fi
-
-cd  /etc/yum.repos.d/
-
-if !(wget $sourcerepo)
-then
-	echo "erreur download 2"
-	exit 2
-fi 
-
-#yum install
-
-echo "perl"
-yum -y install gcc make perl
-echo "kernel"
-yum -y install kernel-devel-3.10.0-957.10.1.el7x86_64
-echo "gcc"
-yum -y install gcc dkms make qt libgomp patch 
-echo "kernel-headers"
-yum -y install kernel-headers kernel-devel binutils glibc-headers glibc-devel font-forge
-echo "virtualbox"
-yum -y install VirtualBox-6.0
-echo "rm"
-rm oracle_vbox.asc
-
-/sbin/rcvboxdrv setup
+yum -y install $source
--- projet_perso/vagrant/installvagrant2.sh
+++ projet_perso/vagrant/installvagrant2.sh
@@ -10,55 +10,14 @@
 #######################################################################################################################
 
 ##Déclaration des variables.
-
-
-#Indicator for download source
-havetowget=0
-
-#Source
-source="https://www.virtualbox.org/download/oracle_vbox.asc"
-sourcerepo="https://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo"
-adresse=`pwd`
-
-echo "(wget -q $source -O- | rpm --import -)"
-if !(wget $source)
-then 
-	echo "erreur download"
-	exit 1
+echo "creation de fichier"
+if !(ls ~/vagrant-centos-7)
+then	
+	mkdir ~/vagrant-centos-7
 fi
 
-if !(rpm --import oracle_vbox.asc)
-then
-	echo "erreur rpm"
-	exit 3
-fi
-
-if !(ls /etc/yum.repo.d/)
-then
-	mkdir  /etc/yum.repo.d/
-fi
-
-cd  /etc/yum.repos.d/
-
-if !(wget $sourcerepo)
-then
-	echo "erreur download 2"
-	exit 2
-fi 
-
-#yum install
-
-echo "perl"
-yum -y install gcc make perl
-echo "kernel"
-yum -y install kernel-devel-3.10.0-957.10.1.el7x86_64
-echo "gcc"
-yum -y install gcc dkms make qt libgomp patch 
-echo "kernel-headers"
-yum -y install kernel-headers kernel-devel binutils glibc-headers glibc-devel font-forge
-echo "virtualbox"
-yum -y install VirtualBox-6.0
-echo "rm"
-rm oracle_vbox.asc
-
-/sbin/rcvboxdrv setup
+cd ~/vagrant-centos-7
+echo "vagrant init"
+vagrant init centos/7
+echo "vagrant up"
+vagrant up
