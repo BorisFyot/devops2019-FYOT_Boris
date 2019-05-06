cec11b392fc731c9836ad55674fe7c7c7ef32d94
root <root@localhost.localdomain>
2019-05-06 11:35:27 +0200

    ajout install docker

---
+++ projet_perso/docker/installDOCKER.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#######################################################################################################################
+#	V1 28/03/2019 : création du programme.                                                                        #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#	                                                                                                              #
+#######################################################################################################################
+
+##Déclaration des variables.
+yum install -y yum-utils device-mapper-persistent-data lvm2
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce 
+systemctl enable docker 
+systemctl start docker 
+systemctl status docker 
